1e9b27dbf4d8deeace9e1a77e03052ec1c1f1718
Kishor Nishad <kishorr127.0@gmail.com>
2024-08-03 12:44:28 +0530

    Refactor auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+const communityAdmin = process.env.COMMUNITY_ADMIN;
+
+const adminMiddleware = (req, res, next)=>{
+    const {admin} = req.body;
+    if(!admin || admin !== communityAdmin){
+        return res.status(404).json({message:'You are not authorized to access users data'});
+    }
+    next();
+}
+
 // Define routes
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/', adminMiddleware, userController.getAllUsers);
+router.get('/:id', adminMiddleware, userController.getUserById);
+router.put('/:id', adminMiddleware, userController.updateUser);
+router.delete('/:id', adminMiddleware, userController.deleteUser);
 
 module.exports = router;
